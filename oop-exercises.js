dd28a24b76cd658d787a60d3b50a911edaa0aed2
niyobertin <niyonkurubbertin@gmail.com>
2021-08-22 03:06:42 -0700

    new work

--- oop-exercises.js
+++ oop-exercises.js
@@ -1,7 +1,7 @@
 console.log('oop woks result');
 //1the cat
 //creation of propreties that represent the cat
-class Animale {
+class Animal {
     constructor(name,tiredness,hunger,lonliness,happiness){
         this.name=name;
      this.tiredness=tiredness;
@@ -15,38 +15,39 @@ class Animale {
     };
     takingFood(){
          this.hunger++;
-         return this.tiredness;
+         return this.hunger;
     };
     living(){
         this.lonliness--;
         return this.lonliness;
-
     };
+    
     gaming(){
         this.happiness++;
-        return this.happiness;
+       return this.happiness;
+
     };
     startus(){
         if(this.tiredness > 50){
-            console.log(`The ${this.name}  is actuary strong now`);
+            console.log(`The ${this.name}  is strong now`);
         }else{
-            console.log(`The ${this.name}  is actuary very tired`);
+            console.log(`The ${this.name}  is  very tired`);
         };
         if(this.hunger < 50){
-            console.log(`The ${this.name}  is actuary very hungry`);
+            console.log(`The ${this.name}  is very hungry`);
         }else{
-            console.log(`The ${this.name}  is actuary has get the food!!`);
+            console.log(`The ${this.name} has get the food!!`);
         };
         if(this.lonliness < 50){
-            console.log(`The ${this.name}  is actuary lonely`);
+            console.log(`The ${this.name}  is lonely`);
 
         }else{
-            console.log(`The ${this.name}  is actuary have a family`);
+            console.log(`The ${this.name}  is have a family`);
         };
         if(this.happiness < 50){
-            console.log(`The ${this.name}  is actuary sud`);
+            console.log(`The ${this.name}  is sud`);
         }else{
-            console.log(`The ${this.name} is actuary very happy!`);
+            console.log(`The ${this.name} is  very happy!`);
         };
     };
   
@@ -54,14 +55,45 @@ class Animale {
 
 
     // calling function
+  
+    let cat = new Animal('Kaboyi',49,39,60,10);
+    cat.sleeping();
+    cat.gaming();
+    cat.takingFood();
+    cat.living();
+    cat.startus();
 
-    let cat = new Animale('Kaboyi',49,39,60,10);
-    console.log(cat.startus());
 
 
     //the leading list
     //An object-oriented book-list!
 
+class BookList{
+    constructor(numOfBookMarkedAsRed,numOfBookNotMarkedAsRed,nextBook,currentBook,[allBook]){
+    this.numOfBookMarkedAsRed = numOfBookMarkedAsRed;
+    this.numOfBookNotMarkedAsRed= numOfBookNotMarkedAsRed;
+    this.nextBook = nextBook;
+    this.currentBook = currentBook;
+    this.allBook = [allBook];
+}
+addBook(book){
+    this.newBook = newBook;
+}
+finishCurrentBook(){
+    
+}
+
+}
+
+class Book{
+    constructor(title,genre,author, read,readDate){
+        this.title = title;
+        THIS.author = author;
+        this.genre = genre;
+        this.read = read;
+        this.readDate = readDate;
+    }
+}
 
 
 
